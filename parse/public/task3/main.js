17d9a4828db14c9186861d38dd410887be82b15a
Andrey Evstropov <genesis.andrey@yandex.ru>
2015-07-24 12:59:26 +0300

    task3 start

---
+++ parse/public/task3/main.js
@@ -0,0 +1,18 @@
+window.AudioContext = window.AudioContext || window.webkitAudioContext;
+var context = new AudioContext();
+
+var audio = document.getElementById('audio');
+
+var source = context.createMediaElementSource(audio);
+source.connect(context.destination);
+
+var createFilter = function (frequency) {
+  var filter = context.createBiquadFilter();
+
+  filter.type = 'peaking'; // тип фильтра
+  filter.frequency.value = frequency; // частота
+  filter.Q.value = 1; // Q-factor
+  filter.gain.value = 0;
+
+  return filter;
+};
