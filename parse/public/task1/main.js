e56dd062fbdc52de7141672ed8d5340049c81900
Andrey Evstropov <genesis.andrey@yandex.ru>
2015-07-29 18:36:14 +0300

    fix header row

---
+++ parse/public/task1/main.js
@@ -0,0 +1,13 @@
+window.onload = function() {
+  var el = document.querySelector('.avia-fixed');
+  if (!window.getComputedStyle(el).position.match('sticky')) {
+    var offsetTop = el.offsetParent.offsetTop;
+    window.addEventListener('scroll', function () {
+      if (document.body.scrollTop > offsetTop) {
+        el.classList.add('avia-fixed_unsupported');
+      } else {
+        el.classList.remove('avia-fixed_unsupported')
+      }
+    });
+  }
+};
